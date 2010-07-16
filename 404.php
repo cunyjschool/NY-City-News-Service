634f0fecac95977dffda0a49d215b8ffca38d102
Daniel Bachhuber <danielbachhuber@gmail.com>
2010-07-16 05:46:43 -0700

    Fixed broken 404 page

--- 404.php
+++ 404.php
@@ -1,11 +1,17 @@
 <?php get_header(); ?>
 
-	<div id="content" class="narrowcolumn">
+	<div id="content" class="clearfix">
+    
+    <div class="post">
 
 		<h2 class="center">Error 404 - Not Found</h2>
+		
+		<p>We're sorry but the page you're looking for isn't here.</p>
+		
+    </div>
+
+    <?php include (TEMPLATEPATH . '/category-sidebar.php'); ?>
 
 	</div>
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
