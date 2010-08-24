5870930ed6c071c173e20a98099a35d26a6fd140
Daniel Bachhuber <danielbachhuber@gmail.com>
2010-08-24 15:16:55 +0000

    Updated image lcations in header

--- header.php
+++ header.php
@@ -63,29 +63,29 @@ A student-powered service at the <a href="http://www.journalism.cuny.edu">CUNY G
   </div>
   
   <div id="partner-sites">
-<a href="http://219mag.com/"><img src="<?php bloginfo('template_directory'); ?>/img/219-logo-thumb.jpg" alt="219 Magazine" /></a>
-<a href="http://motthavenherald.com/"><img src="<?php bloginfo('template_directory'); ?>/img/mott-logo-thumb.jpg" alt="Mott Haven Herald" /></a>
-<a href="http://fort-greene.thelocal.nytimes.com/"><img src="/wp-content/uploads/2010/02/local-thumb.jpg" alt="The Local" /></a>
-<a href="http://isnapny.com/"><img src="<?php bloginfo('template_directory'); ?>/img/isnap-logo-thumb.jpg" alt="ISnapNY Photoblog" /></a>
-<a href="http://cuny.tv/series/219west/listen.lasso?year=2009"><img src="<?php bloginfo('template_directory'); ?>/img/219-west-logo.jpg" alt="219 West" /></a>
+<a href="http://219mag.com/"><img src="<?php bloginfo('template_directory'); ?>/img/sites/219-logo-thumb.jpg" alt="219 Magazine" /></a>
+<a href="http://motthavenherald.com/"><img src="<?php bloginfo('template_directory'); ?>/img/sites/mott-logo-thumb.jpg" alt="Mott Haven Herald" /></a>
+<a href="http://fort-greene.thelocal.nytimes.com/"><img src="<?php bloginfo('template_directory'); ?>/img/sites/nyt-local-thumb.jpg" alt="The Local" /></a>
+<a href="http://isnapny.com/"><img src="<?php bloginfo('template_directory'); ?>/img/sites/isnap-logo-thumb.jpg" alt="ISnapNY Photoblog" /></a>
+<a href="http://cuny.tv/series/219west/listen.lasso?year=2009"><img src="<?php bloginfo('template_directory'); ?>/img/sites/219-west-logo.jpg" alt="219 West" /></a>
   </div>
 </div>
 
 <div id="nav">
   <div class="clearfix" id="navwrap">
 <ul>
-  <li id="homenav"><a href="/">Front Page</a></li>
-  <li id="bronxnav"><a href="/category/bronx/">Bronx</a></li>
-  <li id="brooklynnav"><a href="/category/brooklyn/">Brooklyn</a></li>
-  <li id="manhattannav"><a href="/category/manhattan/">Manhattan</a></li>
-  <li id="queensnav"><a href="/category/queens/">Queens</a></li>
-  <li id="statenislandnav"><a href="/category/staten-island/">Staten Island</a></li>
-  <li id="specialprojects"><a href="/special-projects/">Special Projects</a></li>
-  <li id="podcastnav"><a href="/category/audio/">Audio</a></li>
-  <li id="mapnav"><a href="/news-map/">News Map</a></li>
-  <li id="aboutnav"><a href="/about/">About</a></li>
-  <li id="staffnav"><a href="/staff/">Staff</a></li>
-  <li id="rssnav"><a href="/rss-feeds/" style="background: #f28719; no-repeat bottom left;">RSS Feeds</a></li>
+  <li id="homenav"><a href="<?php bloginfo('url'); ?>/">Front Page</a></li>
+  <li id="bronxnav"><a href="<?php bloginfo('url'); ?>/category/bronx/">Bronx</a></li>
+  <li id="brooklynnav"><a href="<?php bloginfo('url'); ?>/category/brooklyn/">Brooklyn</a></li>
+  <li id="manhattannav"><a href="<?php bloginfo('url'); ?>/category/manhattan/">Manhattan</a></li>
+  <li id="queensnav"><a href="<?php bloginfo('url'); ?>/category/queens/">Queens</a></li>
+  <li id="statenislandnav"><a href="<?php bloginfo('url'); ?>/category/staten-island/">Staten Island</a></li>
+  <li id="specialprojects"><a href="<?php bloginfo('url'); ?>/special-projects/">Special Projects</a></li>
+  <li id="podcastnav"><a href="<?php bloginfo('url'); ?>/category/audio/">Audio</a></li>
+  <li id="mapnav"><a href="<?php bloginfo('url'); ?>/news-map/">News Map</a></li>
+  <li id="aboutnav"><a href="<?php bloginfo('url'); ?>/about/">About</a></li>
+  <li id="staffnav"><a href="<?php bloginfo('url'); ?>/staff/">Staff</a></li>
+  <li id="rssnav"><a href="<?php bloginfo('url'); ?>/rss-feeds/" style="background: #f28719; no-repeat bottom left;">RSS Feeds</a></li>
 </ul>
   </div>
 </div>
