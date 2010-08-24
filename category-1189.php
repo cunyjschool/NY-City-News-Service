77fe84885d121f4e259f31b34ad9093122e64c2d
Daniel Bachhuber <danielbachhuber@gmail.com>
2010-08-24 17:14:26 +0000

    Fixed issue with project loop

--- category-1189.php
+++ category-1189.php
@@ -43,8 +43,6 @@
 			}	
 		}
   ?>
-  <?php else: ?>
-<?php endif ?>
 
 
 				<h3 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h3>
