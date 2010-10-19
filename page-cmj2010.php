bdd9daced04111068e1f2df112754086d256369b
Joe Filippazzo <joe@dev.opress.org>
2010-10-19 18:59:38 +0000

    Cleaned up the php for the custom fields in the cmj template.

--- page-cmj2010.php
+++ page-cmj2010.php
@@ -13,11 +13,12 @@ Template Name: Special Project Template
       <?php query_posts("category_name=cmj-2010"); ?>
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
       	  <div class="post-special-event">
-      	    <?php if(get_post_meta($post->ID, band_date, true) != "") { ?><div class="show_date"><small>Date: <?php echo get_post_meta( $post->ID,"band_date", $single=true ) ; ?></small></div><?php } ?>
+      	    <?php if( $band_date = get_post_meta($post->ID, 'band_date', true) ) { ?><div class="show_date"><small>Date: <?php echo $band_date; ?></small></div><?php } ?>
             <div id="band-photo">
               <h2 class="special-project-post-title" id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
               <a href="<?php the_permalink(); ?>">
                 <?php
+                if ( function_exists('yapb_get_thumbnail') ) {
                                   echo yapb_get_thumbnail(
                                     '', // HTML before image tag
                                     array(
@@ -28,15 +29,16 @@ Template Name: Special Project Template
                                     array('w=315', 'h=236', 'q=100', 'zc=1'), // phpThumb configuration parameters
                                     ''             // image tag custom css class
                                   );
+                  }
                                 ?>
               </a>
             </div>
             <div class="entry">
               <ul class="band-details">
-                <?php if(get_post_meta($post->ID, band_venue, true) != "") { ?><li><small>Venue: <?php echo get_post_meta( $post->ID,"band_venue", $single=true ) ; ?></small></li><?php } ?>
-                <?php if(get_post_meta($post->ID, band_genre, true) != "") { ?><li><small>Genre: <?php echo get_post_meta( $post->ID,"band_genre", $single=true ) ; ?></small></li><?php } ?>
+                <?php if( $band_venue = get_post_meta($post->ID, 'band_venue', true) ) { ?><li><small>Venue: <?php echo $band_venue; ?></small></li><?php } ?>
+                <?php if( $band_genre = get_post_meta($post->ID, 'band_genre', true) ) { ?><li><small>Genre: <?php echo $band_genre; ?></small></li><?php } ?>
               </ul>
-              <span class="byline">By <?php the_author_posts_link('namefl'); ?><?php if(get_post_meta($post->ID, add_author, true) != "") { ?>, <?php echo get_post_meta( $post->ID,"add_author", $single=true ) ; ?><?php } ?><?php if(get_post_meta($post->ID, add_author2, true) != "") { ?>, <?php echo get_post_meta( $post->ID,"add_author2", $single=true ) ; ?><?php } ?></span>
+              <span class="byline">By <?php the_author_posts_link('namefl'); ?><?php if($add_author = get_post_meta($post->ID, 'add_author', true) ) { ?>, <?php echo $add_author; ?><?php } ?><?php if($add_author2 = get_post_meta($post->ID, 'add_author2', true) ) { ?>, <?php echo $add_author2; ?><?php } ?></span>
               <?php the_excerpt() ?>
               <p><a href="<?php the_permalink() ?>" title="Permanent Link to <?php the_title_attribute(); ?>">Read More</a></p>
             </div>
@@ -45,9 +47,9 @@ Template Name: Special Project Template
           <?php endif; wp_reset_query(); ?>
   </div>
   <div id="sidebar-special">
-    <h1><?php if(get_post_meta($post->ID, subhead_1, true) != "") { ?><?php echo get_post_meta( $post->ID,"subhead_1", $single=true ) ; ?><?php } ?></h1>
-    <h1><?php if(get_post_meta($post->ID, subhead_2, true) != "") { ?><?php echo get_post_meta( $post->ID,"subhead_2", $single=true ) ; ?><?php } ?></h1>
-    <h1><?php if(get_post_meta($post->ID, subhead_3, true) != "") { ?><?php echo get_post_meta( $post->ID,"subhead_3", $single=true ) ; ?><?php } ?></h1>
+    <h1><?php if( $subhead_1 = get_post_meta($post->ID, 'subhead_1', true) ) { ?><?php echo $subhead_1; ?><?php } ?></h1>
+    <h1><?php if( $subhead_1 = get_post_meta($post->ID, 'subhead_2', true) ) { ?><?php echo $subhead_2; ?><?php } ?></h1>
+    <h1><?php if( $subhead_1 = get_post_meta($post->ID, 'subhead_3', true) ) { ?><?php echo $subhead_3; ?><?php } ?></h1>
     <?php the_content(); ?>
   </div>
 </div>
