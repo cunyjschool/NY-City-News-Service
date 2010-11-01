0e248eb3556cd3ddd43d97d2e1edf0d9edef0e0f
Daniel Bachhuber <danielbachhuber@gmail.com>
2010-11-01 20:26:49 +0000

    More work styling the election 2010 page

--- js/election2010.js
+++ js/election2010.js
@@ -5,8 +5,13 @@ jQuery(document).ready(function() {
 			jQuery(this).parent().removeClass('active');
 		} else {
 			jQuery('li.mug-shot-link').removeClass('active');
-			jQuery(this).parent().addClass('active');			
+			jQuery(this).parent().addClass('active');	
+			jQuery('#sidebar-election2010 #intro').hide();
+			var media_content = jQuery(this).parent().find('.content-single').html();
+			jQuery('#sidebar-election2010 #content-single-zone').empty().html(media_content);
+			jQuery('#sidebar-election2010 #content-single-zone').show();
 		}
+		return false;		
 	});
 	
 	
@@ -15,15 +20,22 @@ jQuery(document).ready(function() {
 		jQuery(this).addClass('active');
 		var filter = jQuery(this).attr('id');
 		jQuery('li.mug-shot-link').removeClass('active-filter');
-		jQuery('li.mug-shot-link').removeClass('hidden-filter');		
+		jQuery('li.mug-shot-link').removeClass('hidden-filter');
 		jQuery('li.mug-shot-link').each(function(index) {
 			if ( jQuery(this).hasClass(filter) ) {
-				jQuery(this).addClass('active-filter')
+				jQuery(this).addClass('active-filter');
 			} else {
-				jQuery(this).addClass('hidden-filter')
+				jQuery(this).addClass('hidden-filter');
 			}
 		});
-		
+		return false;
+	});
+	
+	jQuery('dd.filters-list a.reset-filters').click(function() {
+		jQuery('dd.filters-list a.filter').removeClass('active');
+		jQuery('li.mug-shot-link').removeClass('active-filter');
+		jQuery('li.mug-shot-link').removeClass('hidden-filter');
+		return false;		
 	});
 	
 });
--- page-election2010.php
+++ page-election2010.php
@@ -9,8 +9,6 @@ Template Name: Special Project - Election 2010
 <div id="content" class="clearfix">
 	
     <div id="election2010" class="page">
-	
-		<h1><?php the_title(); ?></h1>
 		
 		<div id="mosaic">
 			<?php
@@ -19,17 +17,20 @@ Template Name: Special Project - Election 2010
 			
 				$election_mosaic = new WP_Query( $args );
 				$all_metadata = array();
+				$row_counter = 1;
+				$column_counter = 1;
 			
 			?>
 				<?php if ( $election_mosaic->have_posts() ) : ?>
 				<ul class="mug-shot-list clearfix">					
 				<?php while ( $election_mosaic->have_posts() ) : $election_mosaic->the_post(); ?>
 					<?php
+						
 						$meta = array();
 						// Get all of the data from the post meta fields
 						$meta['first_time_voter'] = get_post_meta( get_the_id(), 'first_time_voter', true );
 						$meta['the_age'] = get_post_meta( get_the_id(), 'the_age', true );
-						$meta['gender'] = get_post_meta( get_the_id(), 'gender', true );			
+						$meta['the_gender'] = get_post_meta( get_the_id(), 'the_gender', true );			
 						$meta['the_nabe'] = get_post_meta( get_the_id(), 'the_nabe', true );
 						$meta['the_occupation'] = get_post_meta( get_the_id(), 'the_occupation', true );
 						$meta['the_party'] = get_post_meta( get_the_id(), 'the_party', true );
@@ -37,28 +38,39 @@ Template Name: Special Project - Election 2010
 						// Add metadata to our counts of posts
 						$all_filters['first_time_voter'][$meta['first_time_voter']][] = get_the_id();
 						$all_filters['the_age'][$meta['the_age']][] = get_the_id();
-						$all_filters['gender'][$meta['gender']][] = get_the_id();
+						$all_filters['the_gender'][$meta['the_gender']][] = get_the_id();
 						$all_filters['the_nabe'][$meta['the_nabe']][] = get_the_id();
 						$all_filters['the_party'][$meta['the_party']][] = get_the_id();
 						
+						if ( $row_counter <= 3 && $column_counter <= 3 ) {
+							$bg_color = 'blue';
+						} else if ( $column_counter & 1 ) {
+							$bg_color = 'white';
+						} else {
+							$bg_color = 'red';
+						}
+						
 						// Adding the meta fields is what drives our filtering										
 					?>
-					<li class="mug-shot-link<?php echo ' ' . implode( ' ', $meta ); ?>">
+					<li class="mug-shot-link<?php echo ' ' . implode( ' ', $meta ); echo ' ' . $bg_color; ?>">
 					<?php the_post_thumbnail( 'election-2010-thumb' ); ?>
 					<div class="content-single">
 						<h3><?php the_title(); ?></h3>
-						<p class="byline-pubdate"><?php the_author(); ?></p>
-							<?php the_content(); ?>
-						<p class="meta">
-							
-						</p>
+						<div class="meta">First time voter: 
+						<?php the_content(); ?>
+						<div class="byline-pubdate">By <?php the_author_posts_link(); ?></div>						
 					</div>
 					</li>
-				<?php endwhile;?>
+				<?php 
+					$column_counter++;
+					if ( $column_counter == 8 ) {
+						$row_counter++;
+						$column_counter = 1;
+					}
+				endwhile;?>
 				</ul>					
 				<?php endif; ?>
 		</div><!-- END - #mosaic -->
-		<?php if ( class_exists('GeoMashup') ) { echo GeoMashup::map('zoom=11&height=500&width=650&auto_info_open=false&add_overview_control=false&add_map_type_control=false&map_content=global&map_cat=election-2010');	} ?>
 	</div>
 	<div id="sidebar-election2010" class="sidebar">
 		
@@ -66,15 +78,21 @@ Template Name: Special Project - Election 2010
 			
 		</div>
 		
-		<div class="intro">
-			<h3>Intro</h3>
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); the_content(); endwhile; endif; ?>
+		<div id="intro">
+			<?php if ( have_posts() ) : 
+				while ( have_posts() ) : the_post();
+				echo '<h3>' . get_the_title() . '</h3>';
+				the_content();
+				endwhile;
+				endif; ?>
 		</div>	
 		
-		<div class="filters">
+		<div id="filters">
 		<h3>Filters</h3>
 		<dl>
-			<?php foreach ( $all_filters as $key => $filters ):
+			<?php
+			if ( isset( $all_filters ) ):
+			foreach ( $all_filters as $key => $filters ):
 				$filter_type_label = '';
 				switch ( $key ):
 					case 'first_time_voter':
@@ -83,7 +101,7 @@ Template Name: Special Project - Election 2010
 					case 'the_age':
 						$filter_type_label = 'Age';
 						break;
-					case 'gender':
+					case 'the_gender':
 						$filter_type_label = 'Gender';
 						break;
 					case 'the_nabe':
@@ -107,24 +125,12 @@ Template Name: Special Project - Election 2010
 				echo '</dd>';
 			
 				endforeach;
+				echo '<dt class="filter-label"></dt><dd class="filters-list"><a href="#" class="reset-filters">Reset filters</a></dd>';
+				endif;
 			?>
 		</dl>
 		</div>
 	
-		<h3>Featured Posts</h3>
-		<?php if ( have_posts() ) : ?>
-		<?php $featuredPosts = new WP_Query(); $featuredPosts->query("posts_per_page=15&category_name=election-2010-feature&paged=$paged"); ?>
-			<?php while ($featuredPosts->have_posts()) : $featuredPosts->the_post(); ?>
-				<ul class="mug-shot-list-feature">
-					<li class="mug-shot-link-feature">
-						<div>
-							<a href="<?php the_permalink(); ?>"><img class="size-medium wp-image-73" title="Antennae" src="http://nycitynewsservice.com/files/2010/10/Saturn-Cassini.jpg" alt="" /></a>
-							<h2><?php the_title(); ?></h2>
-							<?php the_excerpt(); ?>
-						</div>
-					</li>
-				</ul>
-			<?php endwhile; endif; ?>
 	</div>
 </div>
 <?php get_footer(); ?>
