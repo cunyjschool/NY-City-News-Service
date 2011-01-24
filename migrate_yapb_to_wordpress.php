bc3077d0afc3bf99fbf85349a9820a4a24ee67ef
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-01-24 22:00:20 +0000

    New script for migrating YAPB photos to the proper way of doing it

---
+++ migrate_yapb_to_wordpress.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * migrate_yapb_to_wordpress.php
+ * Migrate any and all yapb thumbnails to WordPress' featured posts functionality
+ */
+
+require_once( '../../../wp-load.php' );
+
+// Get all of the image thumbnails from the database.
+// For each thumbnail, add it as an attachment if it isn't already
+// Set it as a featured image
+
+
+?>
