d2aa34f6738ab55a7bf217119eda1ab78f60a8e8
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-01-27 19:11:25 +0000

    Breadcrumb back to staff

--- page-class_members.php
+++ page-class_members.php
@@ -12,6 +12,10 @@ global $wpdb, $bp;
 
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<div class="post" id="post-<?php the_ID(); ?>">
+			
+			<div class="breadcrumb">
+				<a href="<?php bloginfo('url'); ?>">&larr; Back to staff</a>
+			</div>
 		<h2><?php the_title(); ?></h2>
 			<div class="entry">
 			
