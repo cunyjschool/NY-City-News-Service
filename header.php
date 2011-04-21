00259b81e8a5fcec8334e8aade9c0c675f883444
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-04-21 14:47:08 +0000

    First pass at new primary navigation

--- header.php
+++ header.php
@@ -46,42 +46,34 @@ A student-powered service at the <a href="http://www.journalism.cuny.edu">CUNY G
   </div>
   
   <div id="search">
-<?php include (TEMPLATEPATH . '/searchform.php'); ?>
+	<?php include (TEMPLATEPATH . '/searchform.php'); ?>
   </div>
 </div>
 
 <div class="wrap clearfix" id="globalwrap">
-  <div style="float: left; width: 476px;">
-<a href="/"><img src="<?php bloginfo('template_directory'); ?>/img/logo2.jpg" alt="NY City News Service" id="logo" /></a>
-  </div>
-  
-  <div id="partner-sites">
-<a href="http://219mag.com/"><img src="<?php bloginfo('template_directory'); ?>/img/sites/219-logo-thumb.jpg" alt="219 Magazine" /></a>
-<a href="http://motthavenherald.com/"><img src="<?php bloginfo('template_directory'); ?>/img/sites/mott-logo-thumb.jpg" alt="Mott Haven Herald" /></a>
-<a href="http://fort-greene.thelocal.nytimes.com/"><img src="<?php bloginfo('template_directory'); ?>/img/sites/nyt-local-thumb.jpg" alt="The Local" /></a>
-<a href="http://isnapny.com/"><img src="<?php bloginfo('template_directory'); ?>/img/sites/isnap-logo-thumb.jpg" alt="ISnapNY Photoblog" /></a>
-<a href="http://219tvmagazine.journalism.cuny.edu/"><img src="<?php bloginfo('template_directory'); ?>/img/sites/219-west-logo.jpg" alt="219 West" /></a>
-  </div>
+	
+	<div class="logo-wrap">
+		<a href="<?php bloginfo( 'url' ); ?>"><img src="<?php bloginfo('template_directory'); ?>/img/logo2.jpg" alt="NY City News Service" id="logo" /></a>
+	</div>
+
+	<div id="partner-sites">
+		<a href="http://219mag.com/"><img src="<?php bloginfo('template_directory'); ?>/img/sites/219-logo-thumb.jpg" alt="219 Magazine" /></a>
+		<a href="http://motthavenherald.com/"><img src="<?php bloginfo('template_directory'); ?>/img/sites/mott-logo-thumb.jpg" alt="Mott Haven Herald" /></a>
+		<a href="http://fort-greene.thelocal.nytimes.com/"><img src="<?php bloginfo('template_directory'); ?>/img/sites/nyt-local-thumb.jpg" alt="The Local" /></a>
+		<a href="http://isnapny.com/"><img src="<?php bloginfo('template_directory'); ?>/img/sites/isnap-logo-thumb.jpg" alt="ISnapNY Photoblog" /></a>
+		<a href="http://219tvmagazine.journalism.cuny.edu/"><img src="<?php bloginfo('template_directory'); ?>/img/sites/219-west-logo.jpg" alt="219 West" /></a>
+	</div><!-- END #partner-sites -->
+	
 </div>
 
-<div id="nav">
-  <div class="clearfix" id="navwrap">
-<ul>
-  <li id="homenav"><a href="<?php bloginfo('url'); ?>/">Front Page</a></li>
-  <li id="bronxnav"><a href="<?php bloginfo('url'); ?>/category/bronx/">Bronx</a></li>
-  <li id="brooklynnav"><a href="<?php bloginfo('url'); ?>/category/brooklyn/">Brooklyn</a></li>
-  <li id="manhattannav"><a href="<?php bloginfo('url'); ?>/category/manhattan/">Manhattan</a></li>
-  <li id="queensnav"><a href="<?php bloginfo('url'); ?>/category/queens/">Queens</a></li>
-  <li id="statenislandnav"><a href="<?php bloginfo('url'); ?>/category/staten-island/">Staten Island</a></li>
-  <li id="specialprojects"><a href="<?php bloginfo('url'); ?>/special-projects/">Special Projects</a></li>
-  <li id="podcastnav"><a href="<?php bloginfo('url'); ?>/category/audio/">Audio</a></li>
-  <li id="mapnav"><a href="<?php bloginfo('url'); ?>/news-map/">News Map</a></li>
-  <li id="aboutnav"><a href="<?php bloginfo('url'); ?>/about/">About</a></li>
-  <li id="staffnav"><a href="<?php bloginfo('url'); ?>/staff/">Staff</a></li>
-  <li id="rssnav"><a href="<?php bloginfo('url'); ?>/rss-feeds/" style="background: #f28719; no-repeat bottom left;">RSS Feeds</a></li>
-</ul>
-  </div>
-</div>
-
- 
+<div id="primary-navigation-wrap" class="wrap">
+	<ul id="primary-navigation" class="navigation inline-navigation">
+		<li class="navigation-topics primary-item"><a href="#">Topics</a></li>
+		<li class="navigation-places primary-item"><a href="#">Places</a></li>
+		<li class="navigation-media primary-item"><a href="#">Media</a></li>
+		<li class="navigation-special-projects primary-item"><a href="#">Special Projects</a></li>
+		<li class="navigation-about secondary-item"><a href="<?php bloginfo( 'url' ); ?>/about/">About</a></li>		
+		<li class="navigation-staff secondary-item"><a href="<?php bloginfo( 'url' ); ?>/staff/">Staff</a></li>		
+	</ul>
+</div><!-- END #primary-navigation-wrap -->
 
