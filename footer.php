c18b457813c850c1741e3cd7d3ee36609b4c6052
Daniel Bachhuber <danielbachhuber@gmail.com>
2011-04-28 22:57:43 +0000

    Link fixes for the footer

--- footer.php
+++ footer.php
@@ -4,13 +4,13 @@
 <a href="http://www.cuny.edu/"><img src="<?php bloginfo('template_directory'); ?>/img/cuny-orange_s50.png" style="float: right;"></a>
 <a href="http://www.journalism.cuny.edu/"><img src="<?php bloginfo('template_directory'); ?>/img/jschool-logo-blue.jpg" style="float: right; margin-right: 20px;"></a>
 
-  <div><a href="http://cuny.edu/">City University of New York</a> | <a href="http://journalism.cuny.edu/">Graduate School of Journalism</a> | <a href="/">New York City News Service</a></div>
+  <div><a href="http://cuny.edu/">City University of New York</a> | <a href="http://journalism.cuny.edu/">Graduate School of Journalism</a> | <a href="<?php bloginfo( 'url' ); ?>">New York City News Service</a></div>
   
   <div style="margin-top: 10px;">
-<strong>INFO</strong>: <a href="/contact-us/">Contact Us</a> | <a href="/about/">About Us</a> | <a href="/staff/">Staff</a><br />
-<strong>MEDIA</strong>: <a href="/rss-feeds/">RSS</a> | <a href="/category/video/">Video</a> | <a href="/category/jstream/">Audio</a> | <a href="/category/multimedia/">Multimedia</a> | <a href="/news-map/">Maps</a><br />
-<strong>BOROUGH</strong>: <a href="/category/bronx/">Bronx</a> | <a href="/category/brooklyn/">Brooklyn</a> | <a href="/category/manhattan/">Manhattan</a> | <a href="/category/queens/">Queens</a> | <a href="/category/staten-island/">Staten Island</a><br />
-<strong>NEWS</strong>: <a href="/category/arts-culture/">Arts &amp; Culture</a> | <a href="/category/business/">Business</a> | <a href="/category/education/">Education</a> | <a href="/category/environment/">Environment</a> | <a href="/category/health/">Health</a> | <a href="/category/housing/">Housing</a> | <a href="/category/politics/">Politics</a> | <a href="/category/public-safety/">Public Safety</a> | <a href="/category/transportation/">Transportation</a><br />
+<strong>INFO</strong>: <a href="<?php bloginfo( 'url' ); ?>/contact-us/">Contact Us</a> | <a href="<?php bloginfo( 'url' ); ?>/about/">About Us</a> | <a href="<?php bloginfo( 'url' ); ?>/staff/">Staff</a><br />
+<strong>MEDIA</strong>: <a href="<?php bloginfo( 'url' ); ?>/rss-feeds/">RSS</a> | <a href="<?php bloginfo( 'url' ); ?>/media/video/">Video</a> | <a href="<?php bloginfo( 'url' ); ?>/media/audio/">Audio</a> | <a href="<?php bloginfo( 'url' ); ?>/media/multimedia/">Multimedia</a> | <a href="<?php bloginfo( 'url' ); ?>/maps/">Maps</a><br />
+<strong>BOROUGH</strong>: <a href="<?php bloginfo( 'url' ); ?>/places/bronx/">Bronx</a> | <a href="<?php bloginfo( 'url' ); ?>/places/brooklyn/">Brooklyn</a> | <a href="<?php bloginfo( 'url' ); ?>/places/manhattan/">Manhattan</a> | <a href="<?php bloginfo( 'url' ); ?>/places/queens/">Queens</a> | <a href="<?php bloginfo( 'url' ); ?>/places/staten-island/">Staten Island</a><br />
+<strong>NEWS</strong>: <a href="<?php bloginfo( 'url' ); ?>/topics/arts-culture/">Arts &amp; Culture</a> | <a href="<?php bloginfo( 'url' ); ?>/topics/business/">Business</a> | <a href="<?php bloginfo( 'url' ); ?>/topics/education/">Education</a> | <a href="<?php bloginfo( 'url' ); ?>/topics/environment/">Environment</a> | <a href="<?php bloginfo( 'url' ); ?>/topics/health/">Health</a> | <a href="<?php bloginfo( 'url' ); ?>/topics/housing/">Housing</a> | <a href="<?php bloginfo( 'url' ); ?>/topics/politics/">Politics</a> | <a href="<?php bloginfo( 'url' ); ?>/topics/public-safety/">Public Safety</a> | <a href="<?php bloginfo( 'url' ); ?>/topics/transportation/">Transportation</a><br />
 <span style="color: #fff;">Icons courtesy <a href="http://www.famfamfam.com/">FamFamFam</a> and <a href="http://www.pinvoke.com/">Pinvoke</a>.</span>
 
 </div>
