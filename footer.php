46de3b77408f1d7e39433cec26e36489b4d78d9c
joe <hover2pi@gmail.com>
2011-07-05 18:12:30 +0000

    Removed Clicky analytics script from the footer.

--- footer.php
+++ footer.php
@@ -16,11 +16,5 @@
 </div>
 <?php wp_footer(); ?>
 
-<!-- Clicky analytics tag -->
-<script src="http://static.getclicky.com/js" type="text/javascript"></script>
-<script type="text/javascript">clicky.init(251938);</script>
-<noscript><p><img alt="Clicky" width="1" height="1" src="http://in.getclicky.com/251938ns.gif" /></p></noscript>
-<!-- End Clicky analytics -->
-
 </body>
 </html>
